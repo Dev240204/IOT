64bea8952efaff1e1ed6401a5aabef279002e0ae
Dev240204 <dev.ace21@sot.pdpu.ac.in>
2024-10-07 19:32:29 +0530

    Production version 1.2

--- routes/sensor.js
+++ routes/sensor.js
@@ -4,14 +4,15 @@ const Data = require("../models/data")
 
 router.post("/data", async (req,res)=>{
     try{
-        const {distance, motion} = req.body
-        
-        const data = await new Data({
-            distance: distance,
-            status: motion
-        })
+        const reqdata = req
+        console.log(reqdata)
 
-        await data.save()
+        // const data = await new Data({
+        //     distance: distance,
+        //     status: motion
+        // })
+
+        // await data.save()
         res.status(200).json({message : "Data entry successful" })
     }catch(err){
         res.status(500).json(`Server error : ${err}`)
